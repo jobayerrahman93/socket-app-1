10c303df6bfcc0d032f157936e12ab964ab3bf2e
jobayercse71 <jobayercse71.com>
2022-06-10 23:00:13 +0600

    setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(
+        `server ${port} is running`
+    )
+})
